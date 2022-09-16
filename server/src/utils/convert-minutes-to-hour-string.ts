444d3ae216df55e8cdb4ca581ecd52a924018259
José Ronaldo <dev.joseronaldo@gmail.com>
2022-09-16 01:02:44 -0300

    :sparkles: feat: Criando todas as rotas do backend

---
+++ server/src/utils/convert-minutes-to-hour-string.ts
@@ -0,0 +1,6 @@
+//1080 -> 18:00
+export function converMinutesToHourString(minutes: number) {
+  const hour = Math.floor(minutes / 60);
+  const minute = minutes % 60;
+  return `${String(hour).padStart(2, "0")}:${String(minute).padStart(2, "0")}`;
+}
