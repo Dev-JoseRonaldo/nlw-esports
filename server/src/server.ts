59309a234a12ece483c1e5683c7352330dcdd112
José Ronaldo <dev.joseronaldo@gmail.com>
2022-09-19 16:10:39 -0300

    refactor: att server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,97 +1,102 @@
 import express from 'express'
-import cors from 'cors'
+import cors from 'cors';
 
-import { PrismaClient } from '@prisma/client'
-
-import { convertHoursStringToMinutes } from './utils/convert-hour-string-to-minutes'
-import { converMinutesToHourString } from './utils/convert-minutes-to-hour-string'
-
-const port = process.env.PORT || 3333
+import { PrismaClient } from '@prisma/client';
+import { convertHoursStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({}))
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
-app.get('/games', async (request, response) => {
-  const games =  await prisma.game.findMany({
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany({
     include: {
-      _count : {
-        select : {
+      _count: {
+        select: {
           ads: true,
         }
       }
     }
-  })
-  return response.json(games)
+  });
+  // res.sendStatus(200).json(games) 
+  return res.status(200).json(games);
 })
 
-app.post('/games/:id/ads', async (request, response) => {
-  const gameId = request.params.id
-  const body = request.body
+app.post('/games/:gameId/ads', async (req, res) => {
+  const gameId: string = req.params.gameId;
+  const body: any = req.body;
 
+  // Validação - zod javascript
   const ad = await prisma.ad.create({
-    data:{
+    data: {
       gameId,
-      name: body.name ,
-      yearsPlaying: body.yearsPlaying ,
-      discord: body.discord ,
-      weekDays: body.weekDays.join(',') ,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
       hourStart: convertHoursStringToMinutes(body.hourStart),
-      hourEnd: convertHoursStringToMinutes(body.hourEnd) ,
-      useVoiceChannel: body.useVoiceChannel ,
-      createdAt: body.createdAt ,
+      hourEnd: convertHoursStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
     }
   })
 
-  return response.status(201).json(ad)
+  return res.status(201).json(ad);
 })
 
-app.get('/games/:id/ads', async (request, response) => {
-  const gameId = request.params.id;
+app.get('/games/:id/ads', async (req, res) => {
+  const gameId: string = req.params.id;
 
   const ads = await prisma.ad.findMany({
     select: {
-      id: true,
-      name: true,
-      weekDays: true,
-      useVoiceChannel: true,
-      yearsPlaying: true,
-      hourStart: true,
-      hourEnd: true,
+        id: true,
+        name: true,
+        weekDays: true,
+        useVoiceChannel: true,
+        yearsPlaying: true,
+        hourStart: true,
+        hourEnd: true,
     },
     where: {
-      gameId: gameId,
+        gameId,
     },
     orderBy: {
-      createdAt: 'desc',
+        createAt: 'desc',
     }
   })
-
-  return response.json(ads.map(ad => {
+  return res.json(ads.map(ad => {
     return {
       ...ad,
       weekDays: ad.weekDays.split(','),
-      hourStart: converMinutesToHourString(ad.hourStart),
-      hourEnd: converMinutesToHourString(ad.hourEnd),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd),
     }
-}))
+  }))
 })
 
-app.get("/ads/:id/discord", async (request, response) => {
-  const adId = request.params.id;
+app.get('/ads/:id/discord', async (req, res) => {
+  const adId = req.params.id;
+  
   const ad = await prisma.ad.findUniqueOrThrow({
     select: {
-      discord: true
+      discord: true,
     },
     where: {
       id: adId,
     }
   })
 
-  return response.json({discord: ad.discord});
-});
+  return res.json({
+    discord: ad.discord,
+  })
+})
 
-app.listen(port)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server Running in port ${PORT}`)
+})
